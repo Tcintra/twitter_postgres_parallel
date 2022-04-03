0b85abc2b7c94b925293d040d8736a5fcdf0b3ef
Tcintra <tcintra@g.hmc.edu>
2022-04-03 16:58:41 -0700

    Updated files

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -17,4 +17,4 @@ time python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:27002/ 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:27003/ --inputs $files
+time python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:33456/ --inputs $files
